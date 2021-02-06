4e681aad2e360aaa46fba3087f6431a0e9117e7c
Feroz <Hasan.Feroz@kantar.com>
2021-02-06 22:11:57 +0600

    day 3 validation

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -1,10 +1,15 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken'); 
+const {validationResult} = require('express-validator');
 //const { use } = require('../routes/auth');
 
 exports.signup = (req, res) =>
 
 {
+
+    // const errors = validationResult(req);
+    // return res.status(400).json({errors: errors.array()})
+
     User.findOne({email: req.body.email})
 .exec((error, user) =>{
     if(user) return res.status(400).json({
